f77c212de8987100a0717ab7e74707b3461834af
Joseph Soria <josephsoria6@gmail.com>
2021-02-04 22:52:34 -0600

    Wrote questions for inquirer

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const inquirer = require(`inquirer`)
 const generateMarkdown = require(`./Develop/utils/generateMarkdown`)
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    `What is the title of your project?`,
+    `What is the description for your project?`,
+    `How do you install your project?`,
+    `What is the usage for your project?`,
+    `What are the contribution guidelines for this project?`,
+    `What are the testing instructions for this application?`,
+    `Does your repository use a license?`,
+    `What license does your repository use?`,
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
