f6e78a26662f99ecc89c5c6138ab4c6dc9f523b1
Joseph Soria <josephsoria6@gmail.com>
2021-02-05 21:30:55 -0600

    Created this file and moved my array of questions

---
+++ questions.js
@@ -0,0 +1,22 @@
+const inquirer = require("inquirer");
+
+// TODO: Create an array of questions for user input
+const questions = [
+    `What is the title of your project?`,
+    `What is the description for your project?`,
+    `How do you install your project?`,
+    `What is the usage for your project?`,
+    `What are the contribution guidelines for this project?`,
+    `What are the testing instructions for this application?`,
+    `Does your repository use a license?`,
+    `What license does your repository use?`,
+];
+
+
+
+
+
+
+
+module.exports
+
