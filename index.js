47a4124f55e58ddc5d76a0642eb7741a716c002d
Joseph Soria <josephsoria6@gmail.com>
2021-02-05 21:29:27 -0600

    Imported required modules and removed questions

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 // TODO: Include packages needed for this application
+const fs = require()
 const inquirer = require(`inquirer`)
-const generateMarkdown = require(`./Develop/utils/generateMarkdown`)
+// const generateMarkdown = require(`./Develop/utils/generateMarkdown`)
+const template = require(`./template`)
+const questions = require(`./questions`)
 
-// TODO: Create an array of questions for user input
-const questions = [
-    `What is the title of your project?`,
-    `What is the description for your project?`,
-    `How do you install your project?`,
-    `What is the usage for your project?`,
-    `What are the contribution guidelines for this project?`,
-    `What are the testing instructions for this application?`,
-    `Does your repository use a license?`,
-    `What license does your repository use?`,
-];
+inquirer
+.prompt({
+    
+})
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
