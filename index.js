8661d6e20e8970a79666e1a62be5811dde958725
Joseph Soria <josephsoria6@gmail.com>
2021-02-05 22:15:58 -0600

    Wrote async functions to ask user questions

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 // TODO: Include packages needed for this application
-const fs = require()
+const fs = require(`fs`)
 const inquirer = require(`inquirer`)
 // const generateMarkdown = require(`./Develop/utils/generateMarkdown`)
 const template = require(`./template`)
 const questions = require(`./questions`)
 
-inquirer
-.prompt({
-    
-})
+async function main() {
+    const userData = await inquirer.prompt(questions);
+
+    console.log(userData);
+}
 
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+main()
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
